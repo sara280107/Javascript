4b386168fd8457f498e6c0c5ce0d4f92d55ea3f9
Sara Patel <patelsara839@gmail.com>
2025-07-04 14:01:00 +0530

    commit

---
+++ Operators.js
@@ -0,0 +1,89 @@
+// // // Arithmetic Operator
+let a1 =10;
+let b1=3;
+
+
+console.log("addition",a1+b1);
+console.log("Subtraction",a1-b1);
+console.log("Multiplication",a1*b1);
+console.log("Division",a1/b1);
+console.log("modulus",a1%b1);
+console.log("Increment",++a1);
+console.log("Decrement",--a1);
+console.log("Exponentiation",a1**2);
+
+
+// //Assignment operator
+let c =5;
+let d=10;
+console.log("Addition",c += 20);
+console.log("Subtraction",d -= 5);
+console.log("Multiplication",d *= 5);
+console.log("Division",d /= 5);
+console.log("Modulus",d %= 5);
+console.log("Exponent",c**= 2);
+
+
+// // comparison operator
+let age =20;
+let result=(age>=18)? "adult":"minor";
+console.log(result);
+let e=15;
+var f=10;
+console.log("equal to",e==f);
+console.log("strict equal to",e===f);
+console.log("greater than",e>f);
+console.log("less than",e<f);
+console.log("greater than equal to",e>=f);
+console.log("less than equal to",e<=f);
+console.log("not equal to",e!=f);
+console.log("strict not  equal to",e!==f);
+
+// // Logical operator
+let g=12;
+let h=4;
+console.log("AND",g>h && g!=h);
+console.log("OR",g>h || g==h);
+console.log("NOT",!g);
+
+// // Bitwise operators
+let i=5;
+let j=10;
+console.log("ADD",i&j);
+console.log("OR",i|j);
+console.log("NOT",~i);
+console.log("XOR",i^j);
+console.log("LEFT SHIFT",i>>1);
+console.log("RIGHT SHIFT",i<<1);
+
+// Ternary operator
+let leap=2024;
+let year = leap%4==0  ? "true":"false" ;
+console.log(year);
+
+// string operations
+
+let Firstname="Sara";
+let Surname="Patel";
+let name= Firstname + Surname;
+console.log(names);
+
+let name1="sara";
+ name1 += "patel";
+console.log(name1);
+
+// typeof
+
+var a="sara";
+var b=12;
+console.log("type:",typeof a);
+console.log("type:",typeof b);
+
+
+
+
+
+
+
+
+
---
+++ Variables.js
@@ -0,0 +1,9 @@
+let names ="sara";
+const name1="Patel";
+var age =18;
+console.log("name",names);
+console.log("surname",name1);
+console.log("age",age);
+
+
+
